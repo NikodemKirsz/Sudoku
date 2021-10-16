0b4ea58ce1baea5d25d2c1d53856e24b8111e0cd
236677 Trela Oskar <236677@edu.p.lodz.pl>
2021-10-16 13:12:51 +0200

    Initial Commit with base project

---
+++ src/main/java/pl/first/firstjava/SudokuBoard.java
@@ -0,0 +1,15 @@
+package pl.first.firstjava;
+
+public class SudokuBoard {
+    private int[][] board;
+
+    public SudokuBoard() {
+        board = new int[9][9];
+
+    }
+
+    public void fillBoard() {
+
+    }
+
+}
---
+++ src/test/java/pl/first/firstjava/SudokuBoardTest.java
@@ -0,0 +1,8 @@
+package pl.first.firstjava;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class SudokuBoardTest {
+
+}
